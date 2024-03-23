2946c6f03aa265b0e5ff2ea53b60879f563e29b1
Djcarrillo6 <djcarrillo6@yahoo.com>
2024-03-22 18:55:47 -0700

    Added TIL section to UI & backend.

---
+++ src/content/til/comparing-godot-development-to-web-development.md
@@ -0,0 +1,11 @@
+---
+author: David Carrillo
+pubDatetime: 2024-03-24
+title: GPU heavy animations in Godot
+postSlug: ""
+featured: false
+draft: false
+tags:
+  - game-development
+description: Today I learned about the GPU heavy animations in Godot.
+---
--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -6,6 +6,9 @@ export const slugifyStr = (str: string) => slugger(str);
 const slugify = (post: CollectionEntry<"blog">["data"]) =>
   post.postSlug ? slugger(post.postSlug) : slugger(post.title);
 
+export const slugifyTils = (post: CollectionEntry<"til">["data"]) =>
+  post.postSlug ? slugger(post.postSlug) : slugger(post.title);
+
 export const slugifyAll = (arr: string[]) => arr.map(str => slugifyStr(str));
 
 export default slugify;
